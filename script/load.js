f080a1907850e926f19a9303857fd41affdfa0db
TheRealKS <koen.sauren@gmail.com>
2017-08-02 18:17:00 +0200

    Added boilerplate for loading

--- script/load.js
+++ script/load.js
@@ -3,5 +3,20 @@ function loadData(file) {
 }
 
 function getData(file) {
-    
+    fetch("getfile.php?file=" + file)
+    .then(function(res) {
+        var ctype = res.headers.get("content-type");
+        if (ctype && ctype.includes("application/json")) {
+            if (res.ok) {
+                return res.json();
+            } else {
+                //Handle error code
+            }
+        }
+        throw new TypeError("Did not receive content-type application/json");
+    })
+    .then(function(json) {
+        //Process json further
+        //loadData(json);
+    });
 }
