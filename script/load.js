2666f312a70a6261a1396eb6ecbe551410691abb
TheRealKS <koen.sauren@gmail.com>
2017-08-04 22:01:11 +0200

    Added first steps for file opening

--- script/load.js
+++ script/load.js
@@ -1,47 +1,98 @@
 function listFiles() {
     fetch("listtests.php")
-    .then(function(res) {
-        var ctype = res.headers.get("content-type");
-        if (ctype && ctype.includes("application/json")) {
-            if (res.ok) {
-                return res.json();
-            } else {
-                //Handle error code
+        .then(function(res) {
+            var ctype = res.headers.get("content-type");
+            if (ctype && ctype.includes("application/json")) {
+                if (res.ok) {
+                    return res.json();
+                } else {
+                    //Handle error code
+                }
             }
-        }
-        throw new TypeError("Did not receive content-type application/json");
-    })
-    .then(function(json) {
-        //Add the files
-    });
+            throw new TypeError("Did not receive content-type application/json");
+        })
+        .then(function(json) {
+            var holder = createFileOpenPanel();
+            var keys = Object.keys(json);
+            var l = keys.length;
+            for (var i = 0; i < l; i++) {
+                var testname = keys[i];
+                var revisions = json[testname];
+                holder.getElementsByClassName("rightpanel_content")[0].appendChild(createFileOpenSubPanel(testname, revisions, i));
+            }
+            document.getElementById("holderright").appendChild(holder);
+        });
+}
+
+function getFileRevisions(test) {
+    fetch("listrevisions.php?test=" + test)
+        .then(function(res) {
+            var ctype = res.headers.get("content-type");
+            if (ctype && ctype.includes("application/json")) {
+                if (res.ok) {
+                    return res.json();
+                } else {
+                    //Handle error code
+                }
+            }
+            throw new TypeError("Did not receive content-type application/json");
+        })
+        .then(function(json) {
+
+        });
 }
 
 function loadData(file) {
     var nameholder = document.getElementById("test_name");
     var questions = document.getElementById("questionholder");
     nameholder.value = file.name;
-    for(var i = 0; i < file.answers.length; i++) {
+    for (var i = 0; i < file.answers.length; i++) {
         addquestion();
     }
-    
+
 }
 
 function getData(file, preload) {
     fetch("getfile.php?file=" + file)
-    .then(function(res) {
-        var ctype = res.headers.get("content-type");
-        if (ctype && ctype.includes("application/json")) {
-            if (res.ok) {
-                return res.json();
-            } else {
-                //Handle error code
+        .then(function(res) {
+            var ctype = res.headers.get("content-type");
+            if (ctype && ctype.includes("application/json")) {
+                if (res.ok) {
+                    return res.json();
+                } else {
+                    //Handle error code
+                }
             }
-        }
-        throw new TypeError("Did not receive content-type application/json");
-    })
-    .then(function(json) {
-        //Process json further
-        //loadData(json);
-        preload ? currenttest = json : loadData(json);
-    });
+            throw new TypeError("Did not receive content-type application/json");
+        })
+        .then(function(json) {
+            //Process json further
+            //loadData(json);
+            preload ? currenttest = json : loadData(json);
+        });
+}
+
+function createFileOpenPanel() {
+    var holder = document.createElement("div");
+    holder.className = "rightpanel";
+    var header = document.createElement("div");
+    header.className = "rightpanel_header";
+    header.innerHTML = "<button type='button' class='showall_icon' title='Show all questions' onclick='collapsePanel(this);'>&nbsp;</button> <span class='rightpanel_header_text'>Load file</span>";
+    var panel = document.createElement("div");
+    panel.className = "rightpanel_content";
+    holder.appendChild(header);
+    holder.appendChild(panel);
+    return holder;
+}
+
+function createFileOpenSubPanel(name, revisions, index) {
+    var hh = document.createElement("div");
+    var h = document.createElement("div");
+    h.className = "rightpanel_subcontentholder";
+    var c = "<button type='button' class='hideall_icon' title='Show all questions' onclick='expandTest(this);'>&nbsp;</button> <span id='testname' class='rightpanel_subcontentholder_content'>" +
+        name + "&emsp;<span class='rightpanel_content_italic'>" + revisions;
+    if (revisions < 2) { c += " revision"; } else { c += " revisions"; }
+    c += "</span></span>";
+    h.innerHTML = c;
+    return h;
 }
