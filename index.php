cb5d2bcc4b890286280ee0d8cb0acf745652cab9
BaasS96 <baass318@gmail.com>
2017-09-03 16:20:34 +0200

    Changed widm test login

--- index.php
+++ index.php
@@ -5,22 +5,29 @@
             Wie Is De Mol
         </title>
         <link rel="stylesheet" type="text/css" href="style/style.css">
-        <script src="script/load.js"></script>
-        <script src="script/test.js"></script>
+        <!--<script src="script/load.js"></script>
+        <script src="script/test.js"></script>!-->
     </head>
     <body id="body">
         <div class="widm-logo-blur-holder">
             <img src="images/WIDM_LOGO_BLACK_BLURRED.jpg" class="widm-logo-blur" alt="" />
         </div>
-        <div id="logon" class="TB_logon">
-            <input id="name" type="text" placeholder="Naam">
-            <br>
-            <input id="pin" type="text" placeholder="Pin">
-            <br>
-            <button onclick="startTest();">Verder</button>
-        </div>
         <div id="content" class="TB contentholder">
-            <div class="TR">
+        <!-- LOGON SCREEN !-->
+            <div class="TR" id="LOGON">
+                <div class="TD logon">
+                    <div>
+                        <input id="name" type="text" placeholder="Naam" class="logon_input">
+                        <br>
+                        <input id="pin" type="text" placeholder="Pin" class="logon_input">
+                        <br>
+                        <button onclick="startTest();" class="logon_submit">Verder</button>
+                    </div>
+                </div>
+            </div>
+        <!-- END !-->
+        <!-- QUESTION !-->
+            <div class="TR" id="QUESTIONA">
                 <div class="TD">
                     <img src="images/WIDM_LOGO_BLACK.jpg" id="WIDM_LOGO" />
                 </div>
@@ -28,9 +35,8 @@
                     <span class="H1">9. &nbsp; Wat is het geluksgetal van de Mol?</span>
                 </div>
             </div>
-            <div class="TR">
+            <div class="TR" id="QUESTIONB">
                 <div class="TD"></div>
-                <div class="TD">
                     <form action="send.php" method="post">
                         <div class="input-bttn-wrapper">
                             <div class="input-bttn-holder">
